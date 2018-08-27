769c0b69f11d8e5797a99c143395bf52e0597895
Avinash M <avinashm201993@gmail.com>
2018-08-27 13:15:48 -0500

    Add project files.

---
+++ Ejoint.CustomerDatabase.Operations/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejoint.CustomerDatabase.Operations
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public char Gender { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
