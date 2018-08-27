a94f11bd3c45fd26be6cb26fe2250b448f28bdd6
Avinash M <avinashm201993@gmail.com>
2018-08-27 13:32:02 -0500

    Created Login class for login table

---
+++ Ejoint.CustomerDatabase.Operations/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejoint.CustomerDatabase.Operations
+{
+    class Login
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int MaxAttempts { get; set; }
+    }
+}
