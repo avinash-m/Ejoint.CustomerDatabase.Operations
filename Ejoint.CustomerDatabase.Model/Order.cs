71a52259b221b1eba5a89359651e0f5d55b12d61
Avinash M <avinashm201993@gmail.com>
2018-08-27 15:18:22 -0500

    Created new model classes for Database model

--- Ejoint.CustomerDatabase.Model/Order.cs
+++ Ejoint.CustomerDatabase.Model/Order.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Ejoint.CustomerDatabase.Operations
+namespace Ejoint.CustomerDatabase.Model
 {
     class Order
     {
