8454cd983c76508ef32869b4b28f22d522cd5f6e
Avinash M <avinashm201993@gmail.com>
2018-08-27 13:29:57 -0500

    Created Payment class for payment table

---
+++ Ejoint.CustomerDatabase.Operations/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejoint.CustomerDatabase.Operations
+{
+    class Payment
+    {
+        public int ID { get; set; }
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public string PaymentType { get; set; }
+        public double AmountPaid { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
