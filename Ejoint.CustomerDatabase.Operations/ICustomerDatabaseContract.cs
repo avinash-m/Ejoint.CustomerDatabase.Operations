5996421279e6572f02b4a9b8b86d04194d25cefd
Avinash M <avinashm201993@gmail.com>
2018-08-27 13:38:31 -0500

    Created ICustomerDatabaseContract interface

---
+++ Ejoint.CustomerDatabase.Operations/ICustomerDatabaseContract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejoint.CustomerDatabase.Operations
+{
+    interface ICustomerDatabaseContract
+    {
+        int SaveCustomer(Customer customer);
+        int UpdateCustomer(int customerID, Customer customer);
+        int DeleteCustomer(int customerID);
+        List<Customer> GetCustomerDetails();
+        Customer GetCustomer(int customerID);
+    }
+}
