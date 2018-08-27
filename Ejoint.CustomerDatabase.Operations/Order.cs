f561b2476507566fff4d587897b369ae4f5cb32f
Avinash M <avinashm201993@gmail.com>
2018-08-27 13:27:34 -0500

    Created Order class for order table

---
+++ Ejoint.CustomerDatabase.Operations/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejoint.CustomerDatabase.Operations
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ItemName { get; set; }
+        public bool IsElectronic { get; set; }
+        public bool IsBook { get; set; }
+        public bool IsMobile { get; set; }
+    }
+}
