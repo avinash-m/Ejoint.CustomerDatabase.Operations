4390812b807d4e3681edf80356d577e3188610ef
Avinash M <avinashm201993@gmail.com>
2018-08-28 18:44:06 -0500

    Changed datatype of Gender from char to string

--- Ejoint.CustomerDatabase.Model/Customer.cs
+++ Ejoint.CustomerDatabase.Model/Customer.cs
@@ -8,7 +8,7 @@ namespace Ejoint.CustomerDatabase.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DOB { get; set; }
-        public char Gender { get; set; }
+        public string Gender { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
         public bool IsActive { get; set; }
